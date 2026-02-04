2b737bd9aa638551e29f8dd3593831734055c144
Jeremi González Perera <gonzalezjeremi532@gmail.com>
2026-02-04 20:29:59 +0000

    Modifiacion general

--- src/es/iess/dam/proyecto/Model/Pedido.java
+++ src/es/iess/dam/proyecto/Model/Pedido.java
@@ -1,40 +1,43 @@
 package es.iess.dam.proyecto.Model;
+
 import es.iess.dam.proyecto.Controller.*;
 
 public class Pedido {
-   private int id;
-   private String nombre;
-   private String Descripcion;
-   private String ingredientes;
-   private double precio;
-   private int cantidad;
-   private double calificacion;
-   private Estado_Del_Pedido estado;
-   private Descuentos descuento;
+  private int id;
+  private String nombre;
+  private String Descripcion;
+  private String ingredientes;
+  private double precio;
+  private int cantidad;
+  private double calificacion;
+  private Estado_Del_Pedido estado;
+  private Descuentos descuento;
 
-   public Pedido(int id, String nombre, String Descripcion, String ingredientes, double precio, int cantidad, double calificacion, Estado_Del_Pedido estado, Descuentos descuento) {
-      this.id = id;
-      this.nombre = nombre;
-      this.Descripcion = Descripcion;
-      this.ingredientes = ingredientes;
-      this.precio = precio;
-      this.cantidad = cantidad;
-      this.calificacion = calificacion;
-      this.estado = estado;
-      this.descuento = descuento;
-   }
-   public Pedido () {
-      this.nombre = "";
-      this.Descripcion = "No Disponible";
-      this.ingredientes = "No Disponible";
-      this.precio = 0.0;
-      this.cantidad = 0;
-      this.calificacion = 0.0;
-      this.estado = Estado_Del_Pedido.NO_DISPONIBLE;
-      this.descuento = Descuentos.NO_DESCUENTO;
-   }
+  public Pedido(int id, String nombre, String Descripcion, String ingredientes, double precio, int cantidad,
+      double calificacion, Estado_Del_Pedido estado, Descuentos descuento) {
+    this.id = id;
+    this.nombre = nombre;
+    this.Descripcion = Descripcion;
+    this.ingredientes = ingredientes;
+    this.precio = precio;
+    this.cantidad = cantidad;
+    this.calificacion = calificacion;
+    this.estado = estado;
+    this.descuento = descuento;
+  }
 
-   public void setNombre(String nombre) {
+  public Pedido() {
+    this.nombre = "";
+    this.Descripcion = "No Disponible";
+    this.ingredientes = "No Disponible";
+    this.precio = 0.0;
+    this.cantidad = 0;
+    this.calificacion = 0.0;
+    this.estado = Estado_Del_Pedido.NO_DISPONIBLE;
+    this.descuento = Descuentos.NO_DESCUENTO;
+  }
+
+  public void setNombre(String nombre) {
     if (nombre != null && !nombre.isEmpty()) {
       this.nombre = nombre;
     }
@@ -46,13 +49,29 @@ public class Pedido {
     }
   }
 
+  public void setIngredientes(String ingredientes) {
+    if (ingredientes != null) {
+      this.ingredientes = ingredientes;
+    }
+  }
+
   public void setPrecio(double precio) {
     if (precio > 0) {
       this.precio = precio;
     }
   }
 
- 
+  public void setCantidad(int cantidad) {
+    if (cantidad > 0) {
+      this.cantidad = cantidad;
+    }
+  }
+
+  public void setCalificacion(double calificacion) {
+    if (calificacion >= 0.0 && calificacion <= 5.0) {
+      this.calificacion = calificacion;
+    }
+  }
 
   public void setEstado(Estado_Del_Pedido estado) {
     if (estado != null) {
@@ -78,6 +97,10 @@ public class Pedido {
     return Descripcion;
   }
 
+  public String getIngredientes() {
+    return ingredientes;
+  }
+
   public double getPrecio() {
     return precio;
   }
@@ -97,5 +120,9 @@ public class Pedido {
   public Descuentos getDescuento() {
     return descuento;
   }
-  
+
+  public double calcularTotal() {
+
+  }
+
 }
